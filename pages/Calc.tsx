4ecc2aec82d6d34f1f47d5213b1f352ecb40c87f
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2022-11-19 16:40:15 +0530

    Component structure

---
+++ pages/Calc.tsx
@@ -0,0 +1,9 @@
+import type { AppProps } from 'next/app'
+import TitleBar from "../components/TitleBar"
+
+
+export default function Calc(): JSX.Element {
+    return (
+    <div><TitleBar /></div>
+    )
+}
