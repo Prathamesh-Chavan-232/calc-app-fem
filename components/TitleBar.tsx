f0c520182ee8731d9d9d304047ea3f0c47dbae59
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2022-11-23 13:14:00 +0530

    Wiped

--- components/TitleBar.tsx
+++
@@ -1,7 +0,0 @@
-import type { AppProps } from 'next/app'
-
-export default function TitleBar(): JSX.Element {
-    return (
-    <div>Calc</div>
-    )
-}
--- pages/Calc.tsx
+++
@@ -1,9 +0,0 @@
-import type { AppProps } from 'next/app'
-import TitleBar from "../components/TitleBar"
-
-
-export default function Calc(): JSX.Element {
-    return (
-    <div><TitleBar /></div>
-    )
-}
