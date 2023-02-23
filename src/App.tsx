57c51125faec648e41a922b89a459911245f5a8e
Prathamesh-Chavan-232 <prathamesh.chavanpsc2018@gmail.com>
2023-02-24 02:54:50 +0530

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './styles/App.scss'
+import "./styles/App.scss";
 
-import Footer from './components/Footer'
-import Numpad from './components/Numpad'
-import TitleBar from './components/TitleBar'
-import ResWindow from './components/ResWindow'
+import Footer from "./components/Footer";
+import Numpad from "./components/Numpad";
+import TitleBar from "./components/TitleBar";
+import ResWindow from "./components/ResWindow";
 
 function App() {
-
-  return (
-    <div className="calc-app">
-      <div className='container'>
-        <TitleBar/>
-        <ResWindow/> 
-        <Numpad/>
-      </div>
-      <Footer/>
-    </div>
-  )
+	return (
+		<div className='calc-app'>
+			<div className='container'>
+				<TitleBar />
+				<ResWindow />
+				<Numpad />
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
-export default App
+export default App;
